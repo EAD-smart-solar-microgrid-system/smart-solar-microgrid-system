ebe31a9316f08dc6090814b08cae456d38a7378d
RavvyS <it23182950@my.sliit.lk>
2026-09-26 03:39:35 +0530

    add zxing barcode scanner dependency

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.zxing)
     implementation(libs.play.services.maps)
     implementation(libs.play.services.location)
     testImplementation(libs.junit)
